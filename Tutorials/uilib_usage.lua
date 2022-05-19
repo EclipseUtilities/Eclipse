6e2b53ec20ba2a31fffe5113945e3dc4784dff10
adam <55650376+qalue@users.noreply.github.com>
2022-05-19 18:14:01 +0100

    Update uilib_usage.lua

--- Tutorials/uilib_usage.lua
+++ Tutorials/uilib_usage.lua
@@ -1,1 +1,6 @@
-tts
+local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/qalue/projects/main/uilibrary.lua", true))()
+--[[
+To CREATE a window put createWindow() (you MUST do this in order for anything to work)
+Afterwards, to add a BUTTON do createButton("Button Text",function() -- code to run here end)
+If you want to use a TOGGLE instead of a button, do createToggle("Toggle Text",function(state) -- code to run here, (state means true or false ) end)
+]]--
